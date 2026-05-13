70accd8029cacc07176e9dc4122a203475a06394
Miguel <mclcastroloureiro@gmail.com>
2026-05-13 15:30:21 -0300

    Adiciona lógica para exibir palpite do usuário nas linhas do grid e estiliza as cores dos resultados

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,7 +5,7 @@ const Grid = ({ currentGuess, guesses, turn }) => {
   return (
     <div>
       {guesses.map((g, i) => {
-        return <Row key={i} />
+        return <Row key={i} guess={g}/>
       })}
     </div>
   )
